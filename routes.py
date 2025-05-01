493c81e85367346d832ac8a8a4599259a3f9cc7b
xanaxhol1c <loram.hoptan@gmail.com>
2025-05-01 22:57:31 +0300

    added create note option

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Request, Depends, Form
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 from todo.database.base import get_db_session
 from fastapi.templating import Jinja2Templates
@@ -18,4 +19,10 @@ async def home(request: Request, db_session : Session = Depends(get_db_session))
     return templates.TemplateResponse('todo/index.html', {"request": request, "app_name": app_name, "notes" : notes})
 
 @router.post("/add")
-async def add(title : str = Form, )
+async def add(title : str = Form(...), text : str = Form(...),  db_session : Session = Depends(get_db_session)):
+    new_note = Note(title=title, text=text)
+    db_session.add(new_note)
+    db_session.commit()
+
+    url = router.url_path_for('home')
+    return RedirectResponse(url=url, status_code=303)
